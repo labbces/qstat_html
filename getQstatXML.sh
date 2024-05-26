fe154fa01e2627613b513d4f4bcec406d921fd81
Diego Mauricio Riaño-Pachón <diriano@gmail.com>
2024-05-25 22:00:53 -0300

    copy to server

--- getQstatXML.sh
+++ getQstatXML.sh
@@ -2,3 +2,4 @@ xmlfile="/home/riano/qstat_html/qstatCluster.xml"
 htmlfile="/home/riano/qstat_html/qstatCluster.html"
 qstat -f -u "*" -xml  > ${xmlfile}
 python3 qstatXML2HTML.py > ${htmlfile}
+scp -P 2222  ${htmlfile} labbces@thevoid:~/www/infra/qstatCluster.html
