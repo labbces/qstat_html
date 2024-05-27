7b5beecce5679c5ed5f2a034c647333333beec9e
Diego M. Riano-Pachon <diriano@gmail.com>
2024-05-26 21:47:28 -0300

    set style file in a variable

--- getQstatXML.sh
+++ getQstatXML.sh
@@ -7,7 +7,8 @@ export SGE_CLUSTER_NAME=bioinfclustr
 export PATH=${SGE_ROOT}/bin/${SGE_ARCH}:$PATH
 xmlfile="/home/riano/qstat_html/qstatCluster.xml"
 htmlfile="/home/riano/qstat_html/qstatCluster.html"
+stylefile="/home/riano/qstat_html/style.css"
 qstat -f -u "*" -xml  > ${xmlfile}
 /Storage/progs/miniconda3/bin/python3 /home/riano/qstat_html/qstatXML2HTML.py > ${htmlfile}
 scp  -i /home/riano/.ssh/id_rsa2 -P 2222  ${htmlfile} labbces@thevoid:~/www/infra/qstatCluster.html
-scp  -i /home/riano/.ssh/id_rsa2 -P 2222  style.css labbces@thevoid:~/www/infra/style.css
+scp  -i /home/riano/.ssh/id_rsa2 -P 2222  ${stylefile} labbces@thevoid:~/www/infra/style.css
