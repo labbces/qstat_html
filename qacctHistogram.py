554db74466ff24fe72139c0d84b34e59ba47562b
Diego Mauricio Riaño-Pachón <diriano@gmail.com>
2024-05-26 22:06:29 -0300

    histogram times

--- qacctHistogram.py
+++ qacctHistogram.py
@@ -6,4 +6,27 @@ lastMod=datetime.datetime.strptime(time.ctime(os.path.getmtime(accFile)), "%a %b
 now=datetime.datetime.now()
 
 differenceTime = now - lastMod
-print(difference)
+print(differenceTime)
+
+pendingTimes=[]
+runningtimes=[]
+
+with open(accFile) as f:
+    lines = f.readlines()
+    for line in lines:
+        if line.startswith('qsub_time'):
+            submitTime=line.split()[1]
+        elif line.startswith('start_time'):
+            startTime=line.split()[1]
+        elif line.startswith('end_time'):
+            endTime=line.split()[1]
+        elif line.startswith('=============================================================='):
+            print(line)
+            pendingTime=datetime.datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%S")-datetime.datetime.strptime(submitTime, "%Y-%m-%dT%H:%M:%S")
+            runnnigTime=datetime.datetime.strptime(endTime, "%Y-%m-%dT%H:%M:%S")-datetime.datetime.strptime(startTime, "%Y-%m-%dT%H:%M:%S")
+            print(f'submit Time:\t{submitTime}\nstart Time:\t{startTime}\nend Time:\t{endTime}\nPending Time:\t{pendingTime}\nRunning Time:\t{runnnigTime}')
+            pendingTimes.append(pendingTime)
+            runningtimes.append(runnnigTime)
+            submitTime=0
+            startTime=0
+            endTime=0
