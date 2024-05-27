9992b724d277268dac70341890c644d81dbde718
Diego M. Riano-Pachon <diriano@gmail.com>
2024-05-27 02:53:00 -0300

    adding histogram copy

--- getQstatXML.sh
+++ getQstatXML.sh
@@ -8,7 +8,9 @@ export PATH=${SGE_ROOT}/bin/${SGE_ARCH}:$PATH
 xmlfile="/home/riano/qstat_html/qstatCluster.xml"
 htmlfile="/home/riano/qstat_html/qstatCluster.html"
 stylefile="/home/riano/qstat_html/style.css"
+histoplot="/home/riano/qstat_html/pending_vs_running_time_log10.png"
 qstat -f -u "*" -xml  > ${xmlfile}
 /Storage/progs/miniconda3/bin/python3 /home/riano/qstat_html/qstatXML2HTML.py > ${htmlfile}
 scp  -i /home/riano/.ssh/id_rsa2 -P 2222  ${htmlfile} labbces@thevoid:~/www/infra/qstatCluster.html
 scp  -i /home/riano/.ssh/id_rsa2 -P 2222  ${stylefile} labbces@thevoid:~/www/infra/style.css
+scp  -i /home/riano/.ssh/id_rsa2 -P 2222  ${histoplot} labbces@thevoid:~/www/infra/pending_vs_running_time_log10.png
