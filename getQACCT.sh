8a38a26c80f22f9c11bc7fb8241fcb377e62fdb2
Diego M. Riano-Pachon <diriano@gmail.com>
2025-07-11 16:17:40 -0300

    activating python environment

--- getQACCT.sh
+++ getQACCT.sh
@@ -1,10 +1,12 @@
 export SGE_CELL=default
 export SGE_ARCH=lx-amd64
 export SGE_ROOT=/Storage/progs/sge-8.1.9/
-export SGE_CLUSTER_NAME=bioinfclustr
-. "/Storage/progs/miniconda3/etc/profile.d/conda.sh"
+export SGE_CLUSTER_NAME=terranova
 
 export PATH=${SGE_ROOT}/bin/${SGE_ARCH}:$PATH
+
+. "/Storage/progs/miniconda3/etc/profile.d/conda.sh"
+source /home/riano/qstat_html/myenv/bin/activate
 
 qacctfile="/home/riano/qstat_html/qAccounting.txt.gz"
 pngfile="/home/riano/qstat_html/pending_vs_running_time_log10.png"
